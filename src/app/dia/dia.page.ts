24089a322a1f9bcf80286ec2ee3b973683c2af34
Antonio <antoniotmll@gmail.com>
2022-03-07 22:21:43 +0100

    implemented ngoninit

--- src/app/dia/dia.page.ts
+++ src/app/dia/dia.page.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dia',
   templateUrl: './dia.page.html',
   styleUrls: ['./dia.page.scss'],
 })
-export class DiaPage {
 
-  constructor(private router: Router) { }
+export class DiaPage implements OnInit{
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   showLunchPage(id): void {
     this.router.navigate(['/comida', id]);
   }
 
+  ngOnInit(): void {
+      const id = this.activatedRoute.snapshot.paramMap.get('id');
+  }
 }
