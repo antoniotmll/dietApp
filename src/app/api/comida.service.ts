0857841750ebb7d2c8de314bdbab90dde1bc4736
Antonio <antoniotmll@gmail.com>
2022-03-08 22:02:32 +0100

    getComida method added

--- src/app/api/comida.service.ts
+++ src/app/api/comida.service.ts
@@ -3,7 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ComidaService {
 
   constructor() { }
+  
+  private comida: {
+    id: string,
+    nombre: string,
+    ingredientes: string
+  };
+
+  getComida() {
+    return this.comida;
+  }
 }
