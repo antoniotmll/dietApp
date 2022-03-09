1056853464a1dea6cd6699f390453b5baa63b722
Antonio <antoniotmll@gmail.com>
2022-03-09 22:33:58 +0100

    finished dia.page.ts

--- src/app/dia/dia.page.ts
+++ src/app/dia/dia.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { ComidaService } from '../api/comida.service';
 
 @Component({
   selector: 'app-dia',
@@ -13,7 +14,10 @@ export class DiaPage implements OnInit{
   listaComida: any[];
 
   constructor(private router: Router,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute,
+              private comida: ComidaService) { 
+                this.listaComida = comida.getComida();
+              }
 
   showLunchPage(id): void {
     this.router.navigate(['/comida', id]);
